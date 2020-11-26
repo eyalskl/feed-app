cecbfe7e9eecf8fd6cf8676ef31f60e7e957fb40
Eyal Barkai <eyalskl18@gmail.com>
2020-11-26 18:43:42 +0200

    deploy

---
+++ precache-manifest.bf8dc8ad2aebfa51e4e969316659dc7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364b33f71a237930c5f1",
+    "url": "/feed-app/css/app.af3ed983.css"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/feed-app/fonts/Questrial-Regular.37c8a1aa.ttf"
+  },
+  {
+    "revision": "7bd0d610a607a9d23b4545e80b0062ad",
+    "url": "/feed-app/index.html"
+  },
+  {
+    "revision": "364b33f71a237930c5f1",
+    "url": "/feed-app/js/app.a2efcc6f.js"
+  },
+  {
+    "revision": "1bbcb49758283a626dd3",
+    "url": "/feed-app/js/chunk-vendors.e5e69024.js"
+  },
+  {
+    "revision": "9f2453f217b3bde836e6f6813845c7b0",
+    "url": "/feed-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/feed-app/robots.txt"
+  }
+]);
